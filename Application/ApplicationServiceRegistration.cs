d30a82023e44e358ea9f42583a358e4b226feb1e
yunus <yunus@DESKTOP-RNAQVAA>
2023-09-06 12:20:15 +0300

    Course 2 completed

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Core.Application.Pipelines.Transaction;
+using Core.Application.Pipelines.Validation;
+using Core.Application.Rules;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +17,40 @@ namespace Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
+
+            // Validation middleware enabled
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
             services.AddMediatR(configuration =>
             {
                 // RegisterServicesFromAssembly -> Mediatr tüm assembly'i tarayarak commands ve response'ları bulup kendi listesine ekler. 
                 // Bu listeye görede ileride bir command isteği gelirse ilgili handler'ı çalıştırır.
-                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()); 
+                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+                // Mediatr'a bir request geldiğinde bunu validation middleware'inden geçirmesini sağlar.
+                configuration.AddOpenBehavior(typeof(RequestValidationBehavior<,>));
+                configuration.AddOpenBehavior(typeof(TransactionScopeBehavior<,>));
             });
 
+            return services;
+        }
+
+        // Assembly'de bulunan tüm BaseBusinessRules sınıflarını arayıp IoC'ye ekler.
+       public static IServiceCollection AddSubClassesOfType(
+           this IServiceCollection services,
+           Assembly assembly,
+           Type type,
+           Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null)
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+                if (addWithLifeCycle == null)
+                    services.AddScoped(item);
+
+                else
+                    addWithLifeCycle(services, type);
             return services;
         }
     }
--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,5 +1,7 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Transaction;
 using Domain.Entities;
 using MediatR;
 using System;
@@ -10,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Commands.Create;
 
-public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>, ITransactionalRequest
 {
     public string Name { get; set; }
 
@@ -18,19 +20,27 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
     {
         private readonly IBrandRepository _brandRepository;
         private readonly IMapper _mapper;
+        private readonly BrandBusinessRules _brandBusinessRules;
 
-        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        public CreateBrandCommandHandler(
+            IBrandRepository brandRepository,
+            IMapper mapper,
+            BrandBusinessRules brandBusinessRules)
         {
             _brandRepository = brandRepository;
             _mapper = mapper;
+            _brandBusinessRules = brandBusinessRules;
         }
 
         public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
+            await _brandBusinessRules.BrandNameCannotBeDuplicatedWhenInserted(request.Name);
+
             Brand brand = _mapper.Map<Brand>(request);
             brand.Id = Guid.NewGuid();
             
             await _brandRepository.AddAsync(brand);
+            //await _brandRepository.AddAsync(brand);
 
             CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(brand);   
             return createdBrandResponse;
